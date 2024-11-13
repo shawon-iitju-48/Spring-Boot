bf51ea2d09cc556874f46456caeda627cdc3b468
Md. Bokhtiar Nadeem Shawon <shawon.iitju.48@gmail.com>
2024-11-14 05:46:06 +0600

    new project on spring

---
+++ src/test/java/com/Shawon/myFirstProjectDay1/MyFirstProjectDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shawon.myFirstProjectDay1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
